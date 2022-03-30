69e9d0eb1d63511852c3c892db1cbaab7a03a230
Snaacky <snaacky@pm.me>
2022-03-29 23:53:20 -0400

    Implemented username lookups, modularized GraphQL queries
    - Usernames are now used instead of using the internal user ID. The internal user ID is still required to access the followers page so it is internally converted from username to ID.
    
    - Modularized sending GraphQL queries into a function that takes query and variables parameters so we can reuse it across the codebase as needed.
    
    - Updated .gitignore so it stops trying to stage my VS Code debug files.

--- giveawaybot/bot.py
+++ giveawaybot/bot.py
@@ -9,11 +9,54 @@ from loguru import logger
 class AniListGiveaway:
     def __init__(self, args):
         self.args = args
-        logger.info(f"Getting contestants from '{self.args.user}' followers list")
+        logger.info(f"Getting contestants from {self.args.user}'s followers list")
+        self.user_id = self.get_user_id()
         self.contestants = self.get_contestants()
         logger.info(f"Found {len(self.contestants)} contestants")
+        logger.info(f"Drawing {self.args.winners} winners")
         self.winners = self.draw_winners()
         logger.info(f"Winner(s): {', '.join(self.winners)}")
+
+    def send_graphql_request(self, query, variables):
+        r = requests.post(
+            url="https://graphql.anilist.co/",
+            json={"query": query, "variables": variables},
+        )
+
+        try:
+            data = r.json()
+        except json.JSONDecodeError:
+            logger.error(f"Failed to get GraphQL API data response: {r.text}")
+            raise SystemExit
+
+        if self.args.debug:
+            logger.debug(f"GraphQL API response: {r.text} [{r.status_code}]")
+
+        if "errors" in data.keys():
+            for message in data["errors"]:
+                logger.error(
+                    f"GraphQL API returned an error: {message['message']} [{r.status_code}]"
+                )
+            raise SystemExit
+
+        return data
+
+    def get_user_id(self):
+        query = """
+        query ($user: String) {
+            User(name: $user) {
+                id
+                name
+            }
+        }
+        """
+        variables = {"user": self.args.user}
+        json = self.send_graphql_request(query=query, variables=variables)
+        if self.args.debug:
+            logger.debug(
+                f"Retrieved {self.args.user}'s internal ID from GraphQL API: {json['data']['User']['id']}"
+            )
+        return json["data"]["User"]["id"]
 
     def get_contestants(self):
         query = """
@@ -35,43 +78,27 @@ class AniListGiveaway:
         contestants = []
         page = 1
         while True:
-            variables = {"userId": self.args.user, "page": page}
-            r = requests.post(
-                url="https://graphql.anilist.co/",
-                json={"query": query, "variables": variables},
-            )
+            variables = {"userId": self.user_id, "page": page}
+            json = self.send_graphql_request(query=query, variables=variables)
 
-            try:
-                r = r.json()
-            except json.JSONDecodeError:
-                logger.error(
-                    f"Failed to get API data for user '{variables['userId']}': {r.text}"
-                )
-                raise SystemExit
-
-            if self.args.debug:
-                logger.debug(f"GraphQL API response: {r}")
-
-            if "errors" in r.keys():
-                for message in r["errors"]:
-                    logger.error(f"GraphQL API returned an error: {message['message']}")
-                raise SystemExit
-
-            if not r["data"]["Page"]["followers"]:
+            if not json["data"]["Page"]["followers"]:
                 logger.error(
                     "Either the AniList user ID specified was invalid or the user has no followers"
                 )
 
-            if len(r["data"]["Page"]["followers"]) < self.args.winners:
+            if json["data"]["Page"]["pageInfo"]["total"] < self.args.winners:
                 logger.error(
-                    "The amount of winners to be drawn is higher than the amount of followers"
+                    (
+                        f"The amount of winners to be drawn ({self.args.winners}) is higher "
+                        f"than the amount of followers ({json['data']['Page']['pageInfo']['total']})"
+                    )
                 )
                 raise SystemExit
 
-            for user in r["data"]["Page"]["followers"]:
+            for user in json["data"]["Page"]["followers"]:
                 contestants.append(user["name"])
 
-            if page >= r["data"]["Page"]["pageInfo"]["lastPage"]:
+            if page >= json["data"]["Page"]["pageInfo"]["lastPage"]:
                 break
 
             page += 1
@@ -87,8 +114,8 @@ if __name__ == "__main__":
     parser.add_argument(
         "-u",
         "--user",
-        help="The ID of the user to draw contestants from",
-        type=int,
+        help="The username of the user to draw contestants from",
+        type=str,
         required=True,
     )
     parser.add_argument(
